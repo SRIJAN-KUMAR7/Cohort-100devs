964c2be824cfaf2d474f1a00af463219f9a5644b
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-06-03 18:49:45 +0530

    express basics done

---
+++ 0-1/w2/index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const bodyParser =require("body-parser");
+
+//middlewares
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+
+
+app.post('/new', function(req, res) {
+    console.log(req.headers)
+    console.log(req.body)
+    res.send("hi there");
+});
+//1,2,3....25565
+app.listen(port, function() {
+    console.log(`App is running on port ${port}`);
+});
+
+
+// see the Postman.png for reference
+
+/* how to handle the post request 
+
+more specifically the body of the post request
+
+//post man returns the json msg 
+
+
+{
+    "msg": "2+2=4"
+}
+
+to log all the headers use req.headers
+
+
+and the output i got is here:
+see headerslogged.png from postman
+
+
+req.body gives undefined
+undefined
+
+
+❓ Why req.body is undefined:
+You haven't added a body-parsing middleware (like express.json()).
+GET requests don't usually have a body, and most clients (like browsers or Postman) don't send one with GET.
+ 
+
+or we can use the body-parser library 
+so install npm i body-parser
+
+now it is done and reference images are : bodyreqfrompostman.png 
+and the mesageraw.png from postman 
+
+
+//Installed nodemon for the rerun and updation of index.js after being changed 
+
+
+learn about --> fetch   
+
+*/
