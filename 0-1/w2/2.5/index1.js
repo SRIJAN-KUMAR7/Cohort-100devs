e6654223026c18b560fb37678b76949383b2eafa
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-06-07 14:24:43 +0530

    expressagain

--- 0-1/w2/2.5/index1.js
+++ 0-1/w2/2.5/index1.js
@@ -2,7 +2,35 @@ const express=require("express");
 
 const app =express();
 
+
+function sum(n){
+    let ans=0;
+    for(let i=0;i<=n;i++){
+        ans+=i;
+    }
+    return ans ;
+}
+
+//req and res => request and response
 app.get('/',function(req,res){
-    console.log("hi there")
+    const n=req.query.n;
+    const ans=sum(n);
+    res.send("hi there Your ans is "+ ans);
 })
-app.listen(3001);
+app.listen(3001,console.log("running on 3001"));
+
+
+/*
+ WHAT is a REQUEST methods?
+
+ 1.GET-> Going for a consultation to get a checkup ..like asking sum till 100
+ 2.POST->Going to get a new kidney inserted ..instagram post similiar 
+ 3.PUT-> Going to get a Kidney replaced 
+ 4.DELETE-> Going to get a Kidney removed 
+
+Status Codes -->
+1.
+2.
+3.
+4.
+*/
